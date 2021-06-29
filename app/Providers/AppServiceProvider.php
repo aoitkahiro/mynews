325669151354dab97f180118416f42217ae69ec3
aoi <aoil@hotmail.co.jp>
2021-06-29 12:01:31 +0000

    PHP/Laravel 19 2021.6.29 Heroku導入

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // HTTPをHTTPS に 通信プロトコルを変更するために以下を追記
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+    }
     }
 }
